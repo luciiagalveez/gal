02b626e97bbb10c9d51c5ec8296b8cb76da6336f
luciiagalveez <luga_0248@hotmail.com>
2020-06-30 12:05:05 +0200

    clase inventario

--- src/main/java/ufv/dis/pareja/Inventario.java
+++ src/main/java/ufv/dis/pareja/Inventario.java
@@ -1,5 +1,25 @@
 package ufv.dis.pareja;
 
+import java.util.ArrayList;
+
+import ufv.dis.pareja.Producto;
+
 public class Inventario {
 
+private ArrayList <Producto> productos;
+	
+	public Inventario() {
+		
+		productos = new ArrayList<Producto>();
+		
+	}
+	
+	public void addProducto(Producto p) {
+		productos.add(p);
+	}
+	
+	public int getListSize() { //metodo para saber el tamaño de la lista (test comprueba si se ha añadido)
+		return productos.size();
+	}
+	
 }
